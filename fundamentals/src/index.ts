751c3ce9bc6b003ce38edd23ad25ab76763c9386
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2024-02-12 23:12:04 -0800

    TS types

--- fundamentals/src/index.ts
+++ fundamentals/src/index.ts
@@ -1,5 +1,16 @@
-let age: number = 20;
-if (age < 50) {
-  age += 10;
-  console.log(age);
+let sales: number = 123_456_789;
+let course: string = "TypeScript";
+let is_published: boolean = true;
+let level; //type any
+
+// function render(doc) { //error:parameter implicitly has an any type
+//     console.log(doc)
+// }
+function render(doc: any) {
+  //annotate as any
+  console.log(doc);
 }
+// let numbers = [1, 2, "3"]; //valid in JS
+// let numbers = [1, 2, 3]; can omit the type annotation
+// let numbers = []; //any type elements
+let numbers: number[] = [];
