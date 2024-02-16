a36369ef080347715e109a42aa5c76005697a311
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2024-02-15 22:35:46 -0800

    typescrip tuples and enums

--- fundamentals/dist/index.js
+++ fundamentals/dist/index.js
@@ -1,7 +1,14 @@
 "use strict";
-let age = 20;
-if (age < 50) {
-    age += 10;
-    console.log(age);
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+function render(doc) {
+    console.log(doc);
 }
+let numbers = [];
+let user = [1, "Iris"];
+console.log(typeof user);
+let mySize = 2;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- fundamentals/src/index.ts
+++ fundamentals/src/index.ts
@@ -14,3 +14,28 @@ function render(doc: any) {
 // let numbers = [1, 2, 3]; can omit the type annotation
 // let numbers = []; //any type elements
 let numbers: number[] = [];
+
+// typescript tuples
+let user: [number, string] = [1, "Iris"];
+console.log(typeof user); // represented in regular array
+
+// typescript enums, represent a list of related constants
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+//PascalCase
+// enum Size {
+//   Small = 1,
+//   Medium,
+//   Large,
+// }
+// let mySize: Size = Size.Medium;
+// console.log(mySize);
+const enum Size { // with const keyword will generate more optimized code
+  Small = 1,
+  Medium,
+  Large,
+}
+let mySize: Size = Size.Medium;
+console.log(mySize);
