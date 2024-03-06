393c9c4ae19591d766c5bcca472b106db4db80ea
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2024-03-05 23:41:56 -0800

    readonly,optional properties,access modifiers and parameter properties

--- classesInterfacesAndOOP/src/index.ts
+++ classesInterfacesAndOOP/src/index.ts
@@ -1,30 +1,42 @@
 class Account {
   // different from JavaScript, need to define properties
-  id: number;
-  owner: string;
-  balance: number;
+  //   readonly id: number; //cannot change readonly property
+  //   owner: string;
+  //   private _balance: number;
+  nickname?: string; //optional property
 
-  constructor(id: number, owner: string, balance: number) {
-    this.id = id;
-    this.owner = owner;
-    this.balance = balance;
-  }
+  //   constructor(id: number, owner: string, balance: number) {
+  //     this.id = id;
+  //     this.owner = owner;
+  //     this._balance = balance;
+  //   }
+  // better way to define constructor,parameter properties
+  constructor(
+    public readonly id: number,
+    public owner: string,
+    private _balance: number
+  ) {}
   deposit(amount: number): void {
     if (amount <= 0) {
       throw new Error("Invalid Amount");
     }
-    this.balance += amount;
+    this._balance += amount;
   }
   withdraw(amount: number): void {
-    if (amount > this.balance) {
+    if (amount > this._balance) {
       throw new Error("Invalid Amount");
     }
-    this.balance -= amount;
+    this._balance -= amount;
+  }
+  getBalance(): number {
+    return this._balance;
   }
 }
 // create object
 let account = new Account(1, "Iris", 0);
 account.deposit(100);
-console.log(account.balance);
+console.log(account.getBalance());
 console.log(account); //object
 console.log(account instanceof Account); //true
+
+// TypeScript access modifiers public private protected
